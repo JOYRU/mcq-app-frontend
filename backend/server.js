3fd0b494c6f2b5033ed6c8c4c1d21f17f00157ca
Joy <joycseru@gmail.com>
2024-11-30 21:04:00 +0600

    changes in index

--- backend/server.js
+++ backend/server.js
@@ -13,16 +13,16 @@ const app = express();
 // const PORT = process.env.PORT || 5010;
 const PORT = 5000 ;
 
-const corsOptions = {
-    origin: '',  // Allow only this origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],     // Allow specific methods
-    allowedHeaders: ['Content-Type', 'Authorization'],  // Allow specific headers
-  };
+// const corsOptions = {
+//     origin: 'http://localhost:5173',  // Allow only this origin
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],     // Allow specific methods
+//     allowedHeaders: ['Content-Type', 'Authorization'],  // Allow specific headers
+//   };
   
-  // Enable CORS with the specified options
-  app.use(cors(corsOptions));
-app.use(cors())
-//app.use(cors());
+//   // Enable CORS with the specified options
+//   app.use(cors(corsOptions));
+//app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
 //  const mongodbURL = process.env.mongodbURL ; 
@@ -34,32 +34,32 @@ app.use(bodyParser.json());
 
  
  // connect to db and run server 
-//  app.listen(PORT,async()=>{   
-//     // console.log('server is running on port') ; 
-//      console.log(`Server is running on http://localhost:${PORT}`);   
-//      await mongoose.connect(mongodbURL) ; 
-//     //connectDB() ; 
-//      console.log('Connection to db Successfully established') ;
-//  })
+ app.listen(PORT,async()=>{   
+    // console.log('server is running on port') ; 
+     console.log(`Server is running on http://localhost:${PORT}`);   
+     await mongoose.connect(mongodbURL) ; 
+    //connectDB() ; 
+     console.log('Connection to db Successfully established') ;
+ })
 
-const connectMongoDB = async () => {
-    try {
-      //const mongoURI = process.env.MONGODB_URI; // Get MongoDB URI from environment variable
-      await mongoose.connect(mongodbURL, {
-       // useNewUrlParser: true,
-        //useUnifiedTopology: true,
-      });
-      console.log('MongoDB Connected!');
-    } catch (err) {
-      console.error('Error connecting to MongoDB:', err);
-      process.exit(1); // Exit the process if connection fails
-    }
-  };
+// const connectMongoDB = async () => {
+//     try {
+//       //const mongoURI = process.env.MONGODB_URI; // Get MongoDB URI from environment variable
+//       await mongoose.connect(mongodbURL, {
+//        // useNewUrlParser: true,
+//         //useUnifiedTopology: true,
+//       });
+//       console.log('MongoDB Connected!');
+//     } catch (err) {
+//       console.error('Error connecting to MongoDB:', err);
+//       process.exit(1); // Exit the process if connection fails
+//     }
+//   };
   
-  // Connect to MongoDB
-  connectMongoDB();
+//   // Connect to MongoDB
+//   connectMongoDB();
 
- app.get('/', (req, res) => {
-    res.send('Hello, world!');
-  });
+//  app.get('/', (req, res) => {
+//     res.send('Hello, world!');
+//   });
   
